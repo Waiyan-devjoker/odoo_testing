44aa7a4a8783a77320602b4be56a358226bcd79b
WaiYan <waiyanmin@innovix-solutions.com>
2023-09-29 15:57:14 +0630

    init

---
+++ testing/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Testing",
+    "version": "1.0.0",
+    "author": "Wai Yan",
+    "license": "LGPL-3",
+    "category": "Testing",
+    "summary": "Testing",
+    "depends": [
+        "base", 'web',
+    ],
+}
